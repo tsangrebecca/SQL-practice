cbd043ad968cdff8666b44c7ae4644e25c0323f1
Rebecca Tsang <109381961+tsangrebecca@users.noreply.github.com>
2025-04-01 15:51:19 -1000

    Add files via upload

---
+++ join_tables.sql
@@ -0,0 +1,10 @@
+Show first name, last name, and the full province name of each patient.
+
+Example: 'Ontario' instead of 'ON'
+
+SELECT
+  first_name,
+  last_name,
+  province_name
+FROM patients
+  JOIN province_names ON province_names.province_id = patients.province_id;
