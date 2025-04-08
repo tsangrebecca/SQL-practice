61fb302841b09e364ee10fdc679c78cd6fde806b
Rebecca Tsang <109381961+tsangrebecca@users.noreply.github.com>
2025-04-07 14:50:05 -1000

    Add files via upload

---
+++ Join_tables_and_filter.sql
@@ -0,0 +1,36 @@
+Show patient_id, first_name, last_name from patients whos diagnosis is 'Dementia'.
+
+Primary diagnosis is stored in the admissions table.
+
+Solution 1:
+SELECT
+  patients.patient_id,
+  first_name,
+  last_name
+FROM patients
+  JOIN admissions ON admissions.patient_id = patients.patient_id
+WHERE diagnosis = 'Dementia';
+
+Solution 2 with subquery:
+SELECT
+  patient_id,
+  first_name,
+  last_name
+FROM patients
+WHERE patient_id IN (
+    SELECT patient_id
+    FROM admissions
+    WHERE diagnosis = 'Dementia'
+  );
+
+Solution 3 with subquery:
+SELECT
+  patient_id,
+  first_name,
+  last_name
+FROM patients p
+WHERE 'Dementia' IN (
+    SELECT diagnosis
+    FROM admissions
+    WHERE admissions.patient_id = p.patient_id
+  );
