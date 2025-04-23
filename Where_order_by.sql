778febbcd8b8a02746838bba892f92013fe01b5d
Rebecca Tsang <109381961+tsangrebecca@users.noreply.github.com>
2025-04-22 15:36:43 -1000

    Add files via upload

---
+++ Where_order_by.sql
@@ -0,0 +1,29 @@
+Show first and last name, allergies from patients which have allergies to either 'Penicillin' or 'Morphine'. Show results ordered ascending by allergies then by first_name then by last_name.
+
+SOLUTION 1:
+SELECT
+  first_name,
+  last_name,
+  allergies
+FROM patients
+WHERE
+  allergies IN ('Penicillin', 'Morphine')
+ORDER BY
+  allergies,
+  first_name,
+  last_name;
+
+SOLUTION 2:
+SELECT
+  first_name,
+  last_name,
+  allergies
+FROM
+  patients
+WHERE
+  allergies = 'Penicillin'
+  OR allergies = 'Morphine'
+ORDER BY
+  allergies ASC,
+  first_name ASC,
+  last_name ASC;
