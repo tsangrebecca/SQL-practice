227f13f968d202ebf2fd4ae7289e8f4dd4c32fb5
Rebecca Tsang <109381961+tsangrebecca@users.noreply.github.com>
2025-04-22 15:37:06 -1000

    Add files via upload

---
+++ Group_by_2columns_having.sql
@@ -0,0 +1,11 @@
+Show patient_id, diagnosis from admissions. Find patients admitted multiple times for the same diagnosis.
+
+Solution:
+SELECT
+  patient_id,
+  diagnosis
+FROM admissions
+GROUP BY
+  patient_id,
+  diagnosis
+HAVING COUNT(*) > 1;
