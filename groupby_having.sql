c25c299a522b3eb0d150b2ea049b71edfc64c239
Rebecca Tsang <109381961+tsangrebecca@users.noreply.github.com>
2025-05-05 14:12:37 -1000

    Add files via upload

---
+++ groupby_having.sql
@@ -0,0 +1,9 @@
+Show the province_id(s), sum of height; where the total sum of its patient's height is greater than or equal to 7,000.
+
+Solution:
+SELECT
+  province_id,
+  SUM(height) AS sum_height
+FROM patients
+GROUP BY province_id
+HAVING sum_height >= 7000;
