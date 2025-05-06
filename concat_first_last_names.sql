2f49a2c0bb67ad36b36a30071d753727dcd2da42
Rebecca Tsang <109381961+tsangrebecca@users.noreply.github.com>
2025-05-05 14:12:25 -1000

    Add files via upload

---
+++ concat_first_last_names.sql
@@ -0,0 +1,14 @@
+We want to display each patient's full name in a single column. Their last_name in all upper letters must appear first, then first_name in all lower case letters. Separate the last_name and first_name with a comma. Order the list by the first_name in decending order
+EX: SMITH,jane
+
+Solution 1:
+SELECT
+  CONCAT(UPPER(last_name), ',', LOWER(first_name)) AS new_name_format
+FROM patients
+ORDER BY first_name DESC;
+
+Solution 2:
+SELECT
+  UPPER(last_name) || ',' || LOWER(first_name) AS new_name_format
+FROM patients
+ORDER BY first_name DESC;
